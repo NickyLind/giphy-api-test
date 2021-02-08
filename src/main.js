f834f96e145ab47f69e82f10248e0a4a6a98f363
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-08 14:07:33 -0600

    add input; add input and api logic
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,4 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './css/styles.css';
+
+$(document).ready(function() {
+  $('#gifSearcher').click(function() {
+    const image = $('#search').val();
+    $('#search').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?q=${image}&api_key=${process.env.API_KEY}&limit=5`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.output').append(`<img src="${response.data[0].embed_url}"></img>`);
+      //$('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
