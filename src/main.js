088a2265cce40524a25cc9b9a7b6cd34327b66ae
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-08 14:58:24 -0800

    add trending gif functionality Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com> Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,7 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $("#trending").click(function)
+  $("#trending").click(function()  {
+    $('.output').empty();
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/trending?&api_key=${process.env.API_KEY}&limit=5`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response)  {
+      const myArray = [0,1,2,3,4];
+      for (let i = 0; i < myArray.length; i++)  {
+        $('.output').append(`<img width="300" height="300" src="${response.data[i].images.original.url}">`);
+      }
+    }
+  });
+  
   $('#gifSearcher').click(function() {
     const image = $('#search').val();
     $('#search').val("");
@@ -25,9 +46,7 @@ $(document).ready(function() {
     function getElements(response) {
       const myArray = [0, 1, 2, 3, 4];
       for (let i = 0; i < myArray.length; i++) {
-      $('.output').append(`<img width="300" height="300" src="${response.data[i].images.original.url}">`);
-      //$('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      // $('.output').append(`${response.data[0].url}`)
+        $('.output').append(`<img width="300" height="300" src="${response.data[i].images.original.url}">`);
       }
     }
   });
